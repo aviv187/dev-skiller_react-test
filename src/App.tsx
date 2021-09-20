d5d62517f2839c2ff5174024a1683f74df59e4a4
aviv187 <avivb35@gmail.com>
2021-09-20 15:21:24 +0300

    creating a skeleton for the store

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/header';
+
+import styles from './scss/app.module.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header></Header>
       </header>
     </div>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../scss/header.module.scss'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      My Store
+    </div>
+  );
+}
+
+export default Header;
