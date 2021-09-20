0e093611eff2ea3229c74a4f306c670f88105666
aviv187 <avivb35@gmail.com>
2021-09-20 16:37:08 +0300

    creating an add product form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CreateProduct from './components/ceatreProduct';
 import Header from './components/header';
 
 import styles from './scss/app.module.scss'
@@ -7,9 +8,8 @@ import styles from './scss/app.module.scss'
 function App() {
   return (
     <div className={styles.App}>
-      <header className="App-header">
-        <Header></Header>
-      </header>
+      <Header></Header>
+      <CreateProduct />
     </div>
   );
 }
---
+++ src/components/ceatreProduct.tsx
@@ -0,0 +1,62 @@
+import React, { ChangeEvent, useRef, useState } from 'react';
+
+import styles from '../scss/form.module.scss'
+
+const CreateProduct = () => {
+  const [img, setImg] = useState<string | undefined>();
+
+  const formRef = useRef<HTMLFormElement>(null)
+
+  const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const imageInput: any = event.target;
+
+    if (imageInput.files && imageInput.files[0]) {
+      const img = imageInput.files[0];
+      setImg(URL.createObjectURL(img));
+    }
+  };
+
+  const saveProduct = () => {
+    const inputs: any = formRef.current!.elements;
+
+    const image = inputs.image.value;
+    const name = inputs.name.value.replace(/\s+/g, ' ').trim();;
+    const description = inputs.description.value.replace(/\s+/g, ' ').trim();;
+    const price = inputs.price.value;
+
+    if (image === '' || name === '' || description === '' || price === '') {
+      alert('All Fields Must be filled');
+      return;
+    }
+
+    console.log(image)
+    console.log(name)
+    console.log(description)
+    console.log(price)
+  }
+
+  return (
+    <form ref={formRef} className={styles.form}>
+      {img && <img src={img} alt='product' />}
+      <div className={styles.row}>
+        <label>Select Image:</label>
+        <input type="file" accept="image/*" name='image' onChange={onImageChange} />
+      </div>
+      <div className={styles.row}>
+        <label>Name: </label>
+        <input name='name' type='text' placeholder='Product Name' />
+      </div>
+      <div className={styles.row}>
+        <label>Description: </label>
+        <input name='description' type='text' placeholder='Product Description' />
+      </div>
+      <div className={styles.row}>
+        <label>Price: </label>
+        <input name='price' type='number' placeholder='Product Price' />
+      </div>
+      <div className={styles.button} onClick={saveProduct}>Save</div>
+    </form>
+  );
+}
+
+export default CreateProduct;
